a9ce843e56b5403337b3c41a2721466dbe84da97
James Trickey <james@goss.media>
2025-06-23 23:02:51 +0100

    effect on mount

--- src/hooks/use-equality-checker.ts
+++ src/hooks/use-equality-checker.ts
@@ -2,12 +2,10 @@ import { useRef } from 'react'
 import { shallowEqualArrays } from '../utils/general'
 
 export const useEqualityChecker = <T>(dependencies: T[]): boolean => {
-  const ref = useRef<T[]>(dependencies)
+  const ref = useRef<T[]>([])
 
-  // Do the comparison synchronously
   const isEqual = shallowEqualArrays(ref.current, dependencies)
 
-  // Update the ref immediately for next render
   ref.current = dependencies
 
   return isEqual
